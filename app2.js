8752323c16b0bc2384e531e5f54d7658b3f341dd
estebancheriff <92827492+estebancheriff@users.noreply.github.com>
2021-10-19 20:21:57 -0300

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,16 @@
+//Creando variables
+var nameUser = "john";
+let lastName = "Carter";
+//reasignando nombres de variables
+nameUser = 'Pedro';
+
+console.log(nameUser)
+console.log (lastName)
+
+//Constante
+const  PI = 3.14;
+
+console.log(PI)
+
+let nombreDePersona = 'Dorito';
+console.log(nombreDePersona)
